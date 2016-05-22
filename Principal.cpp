16533253fa6365a00d53ed3db12772fb72de1e2f
Jade medeiros <jadsamiamedeiros11@gmail.com>
2016-05-22 10:14:28 -0300

    Atualização
    Nova Atualização
    Adicionando um novo arquivo
    -Fazendo um personagem em tela

---
+++ Principal.cpp
@@ -0,0 +1,16 @@
+//Meu primeiro código em C++
+#include <iostream>
+using namespace std;
+
+//Fazendo um personagem em tela
+int main(int argc, char* args[])
+{
+
+	cout << " o  " << endl;
+	cout << "/|\\ " << endl;
+	cout << " /\\" << endl;
+
+	return 0;
+}
+
+
